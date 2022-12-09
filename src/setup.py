ce6a9c5c5bce84761d1e22c83a7fd84ef57ed729
Rafael Nobre <rafaelmedeirosnobre2001@gmail.com>
2022-12-09 10:46:25 -0300

    docs: ajusta readme da distribuição e keywords

--- src/setup.py
+++ src/setup.py
@@ -11,7 +11,7 @@ long_description = readme
 cmds = ["cmd/*.txt"]
 setup(
         name ='ez_docs',
-        version ='0.0.6',
+        version ='0.1.1',
         author ='Bruno Ribeiro, Bruno Martins, Diógenes Dantas, Igor Penha, Lucas Gobbi e Rafael Nobre',
         author_email ='rafaelmedeirosnobre2001@gmail.com',
         description ='Um pacote python de geração de documentos automáticos',
@@ -30,7 +30,7 @@ setup(
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
         ),
-        keywords ='ez_docs',
+        keywords =['ez_docs', 'ez-docs'],
         install_requires = requirements,
         zip_safe = False
 )
