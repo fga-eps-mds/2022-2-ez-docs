b11533b7d9161e179323683c45cf84cab9bd3fbb
LucasBergholz <lucas.bergholz@gmail.com>
2023-01-05 11:27:10 -0300

    tests: Testing the module of the file name #84
    Co-authored-by: Igor Penha <igor.penharol@gmail.com>
    
    Co-authored-by: Bruno Martins <bruno.martval@gmail.com>
    
    Co-authored-by: Rafael Nobre
    <rafaelmedeirosnobre2001@gmail.com>

--- test/test_docGeneration.py
+++ test/test_docGeneration.py
@@ -1,4 +1,24 @@
-from ez_docs.modules.doc_generation import loc_sub
+from ez_docs.modules.doc_generation import *
+
+key_value = {
+    "NOME": "Wallace",
+    "MATRICULA": "20990088"
+}
 
 def test_loc_sub():
     assert loc_sub("Ola <<Chave>>", "Chave", "Mundo")  == "Ola Mundo"
+
+def test_loc_sub_error():
+    assert loc_sub("Ola <<Chave>>", "Chave", "Mundo")  != "Ola Mudo"
+
+def test_slugify():
+    assert slugify("bruno#o:brabo. mago") == "brunoobrabo-mago"
+
+def test_slugify_error():
+    assert slugify("bruno#o:brabo. mago") != "bruno#o:brabo. mago"
+
+def test_recognize_file_name():
+    assert recognize_file_name(key_value, "NOME_MATRICULA") == "wallace_20990088"
+
+def test_recognize_file_name_error():
+    assert recognize_file_name(key_value, "NOME_MATRICULA") != "Wallace_20990088"
